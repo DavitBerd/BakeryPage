304cf93fb8094c42270346490a7951ae8db778d5
Dachi997 <chvleshvilidachi011@gmail.com>
2025-02-13 00:47:33 +0400

    chasworeba

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const swiper = new Swiper('.swiper', {
+/*DATO*/
+/*DATO*/
+/*ANRI*/
+/*ANRI*/
+/*DACHI*/
+const swiper2 = new Swiper('.myswiper', {
     direction: 'horizontal',
     loop: true,
     slidesPerView: 1,
@@ -19,16 +24,15 @@ const swiper = new Swiper('.swiper', {
     }
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.swiper-slide').forEach(slide => {
-        slide.setAttribute('tabindex', '0');
-    });
-
-    document.querySelector('.swiper').addEventListener('click', function (e) {
-        const card = e.target.closest('.SC');
-        if (card) {
-            document.querySelectorAll('.SC').forEach(c => c.classList.remove('active'));
-            card.classList.add('active');
-        }
-    });
+document.querySelectorAll('.swiper-slide').forEach(slide => {
+    slide.setAttribute('tabindex', '0');
 });
+
+document.querySelector('.myswiper').addEventListener('click', function (e) {
+    const card = e.target.closest('.SC');
+    if (card) {
+        document.querySelectorAll('.SC').forEach(c => c.classList.remove('active'));
+        card.classList.add('active');
+    }
+});
+/*DACHI*/ 
