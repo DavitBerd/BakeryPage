11608e1e403d147a445837c2f3a8a870e6029d05
Dachi997 <chvleshvilidachi011@gmail.com>
2025-02-15 04:24:01 +0400

    chasworeba kide

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
 /*DATO*/
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+        },
+    },
+
+});
+document.querySelectorAll('.slidecard').forEach(card => {
+    card.addEventListener('click', () => {
+        document.querySelectorAll('.slidecard').forEach(c => {
+            c.classList.remove('active');
+            c.querySelector('.abs').classList.add('null');
+        });
+        card.classList.add('active');
+        card.querySelector('.abs').classList.remove('null');
+    });
+});
 /*DATO*/
 /*ANRI*/
+const middleSwiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    breakpoints: {
+        768: {
+            slidesPerView: 4,
+        },
+    },
+});
+const swiperContainer = document.querySelector('.swiper-container');
+swiperContainer.addEventListener('click', (e) => {
+    const clickedStep = e.target.closest('.step');
+    if (!clickedStep) return;
+    swiperContainer.querySelectorAll('.step.active').forEach((el) => {
+        el.classList.remove('active');
+    });
+    clickedStep.classList.add('active');
+});
+
 /*ANRI*/
 /*DACHI*/
 const swiper2 = new Swiper('.myswiper', {
